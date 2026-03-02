18b0d1cb50fb4a38dcc87d53dca12ce98ac14f58
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-02 12:24:14 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,21 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
+  // AGAR PATH WEBHOOK HAI, TOH SUPABASE SESSION CHECK MAT KARO
+  if (request.nextUrl.pathname.startsWith('/api/webhook')) {
+    return NextResponse.next()
+  }
+
   return await updateSession(request)
 }
 
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for:
-     * - api/webhook (ISSE EXCLUDE KIYA HAI TAKI META CONNECT HO SAKE)
+     * Match all request paths except for the ones starting with:
      * - _next/static, _next/image, favicon.ico, etc.
      */
-    '/((?!api/webhook|_next/static|_next/image|favicon.ico|assets/email-templates|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|assets/email-templates|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
